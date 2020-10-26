e949425994ce8d68233e9890764f82278d1684b7
Shadee Merhi <shadeemerhi@Shadees-MacBook-Pro-2.local>
2020-10-26 16:36:11 -0600

    DayList component rendering correct styles based on props

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import DayListItem from './DayListItem';
 
 export default function DayList(props) {
 
-  const dayItems = [];
-  return (
-    <ul>
-    </ul>
-  );
+  const days = props.days.map((day) => {
+    return (
+      <DayListItem 
+        key={day.id}
+        name={day.name}
+        spots={day.spots}
+        selected={day.name === props.day}
+        setDay={props.setDay}
+      />
+    );
+  });
+  return days;
 }
