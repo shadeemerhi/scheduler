7e7d534d1e5a076dd495169f31cff1f5dd166832
Shadee Merhi <shadeemerhi@Shadees-MacBook-Pro-2.local>
2020-10-26 16:10:15 -0600

    Added formatSpots function to render proper statement based on spots remaining

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,10 +4,26 @@ const classNames = require('classnames');
 
 export default function DayListItem(props) {
 
+  const formatSpots = function() {
+    if (props.spots === 0) {
+      return 'no spots remaining';
+    } else if (props.spots === 1) {
+      return props.spots + ' spot remaining';
+    } else {
+      return props.spots + ' spots remaining';
+    }
+  }
+
+  let dayClass = classNames({
+    "day-list__item": true,
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+
   return(
-    <li onClick={() => props.setDay(props.name)}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
