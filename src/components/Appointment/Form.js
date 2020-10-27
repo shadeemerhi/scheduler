9ce29fb412781e445f5160698f5c63ff8afac54a
Shadee Merhi <shadeemerhi@Shadees-MacBook-Pro-2.local>
2020-10-27 16:45:28 -0600

    cancel() and save() function added to clear/store interviewer and student name

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -18,6 +18,10 @@ export default function Form(props) {
     props.onCancel();
   }
 
+  const save = function() {
+    props.onSave(name, interviewer);
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -36,7 +40,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={() => cancel()}>Cancel</Button>
-          <Button confirm onClick={props.onSave}>Save</Button>
+          <Button confirm onClick={() => save()}>Save</Button>
         </section>
       </section>
     </main>
