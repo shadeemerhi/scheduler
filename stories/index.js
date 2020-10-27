609ef8e2249b8d476d4a3c063ebab1dac898f3e5
Shadee Merhi <shadeemerhi@Shadees-MacBook-Pro-2.local>
2020-10-26 23:07:39 -0600

    Header component intialized and stories created

--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,7 @@ import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index";
+import Header from "components/Appointment/Header";
 
 
 storiesOf("Button", module)
@@ -140,3 +141,4 @@ storiesOf("InterviewerList", module)
   })
   .add("Appointment", () => <Appointment />)
   .add("Appointment with time", () => <Appointment time={"12pm"}/>)
+  .add("Header", () => <Header time={"12pm"}/>)
