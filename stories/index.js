12c106f9a2cecc421547549f4b9e5256a498b4f2
Shadee Merhi <shadeemerhi@Shadees-MacBook-Pro-2.local>
2020-10-27 14:52:09 -0600

    Created the Form component and stories for Edit and Create Appointments

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 
 storiesOf("Button", module)
@@ -152,3 +153,5 @@ storiesOf("InterviewerList", module)
   .add("Confirm", () => <Confirm message={"Delete the appointment?"} onConfirm={action("onConfirm")} onCancel={action("onCancel")}/>)
   .add("Status", () => <Status message={"Deleting"}/>)
   .add("Error", () => <Error message={"Could not delete appointment"} onClose={action("onClose")}/>)
+  .add("Form Create", () => <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} onChange={action("setInterviewer")}/>)
+  .add("Form Edit", () => <Form name={"Lydia Miller Jones"} interviewers={interviewers} interviewer={interviewers[0]} onChange={action("setInterviewer")} onSave={action("onSave")} onCancel={action("onCancel")}/>)
