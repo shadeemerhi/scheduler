b0635aad59861a97231306dcfe831b93cf32d578
Shadee Merhi <shadeemerhi@Shadees-MacBook-Pro-2.local>
2020-10-28 13:39:49 -0600

    axios request made to scheduler-api, rendering days OK

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,31 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-  {
-    id: 4,
-    name: "Thursday",
-    spots: 3,
-  },
-];
 
 const appointments = [
   {
@@ -76,7 +54,15 @@ const appointments = [
 
 export default function Application(props) {
 
+  const [days, setDays] = useState([]);
   const [day, setDay] = useState("Monday");
+
+  useEffect(() => {
+    axios.get('/api/days').then(response => {
+      console.log(response.data)
+      setDays(response.data);
+    })
+  },[]);
   
   return (
     <main className="layout">
