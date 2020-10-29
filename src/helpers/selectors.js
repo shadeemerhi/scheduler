1f51aa39f480902a64ee5200714a2f92fa55d2c9
Shadee Merhi <shadeemerhi@Shadees-MacBook-Pro-2.local>
2020-10-29 00:21:57 -0600

    Removed hard-coded test data

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -27,19 +27,6 @@ const getAppointmentsForDay = function(state, day) {
   return output;
 }
 
-const interviewers =  {
-  "1": {  
-    "id": 1,
-    "name": "Sylvia Palmer",
-    "avatar": "https://i.imgur.com/LpaY82x.png"
-  },
-  "2": {
-    id: 2,
-    name: "Tori Malcolm",
-    avatar: "https://i.imgur.com/Nmx0Qxo.png"
-  }
-}
-
 const getInterview = function(state, interview) {
   const output = {};
   
