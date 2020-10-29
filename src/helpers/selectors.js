16699847cf4cb6f859ff997fcb51d241fc7084cc
Shadee Merhi <shadeemerhi@Shadees-MacBook-Pro-2.local>
2020-10-28 18:00:21 -0600

    Removed console.log statements

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -3,13 +3,11 @@ const getAppointmentsForDay = function(state, day) {
   let output = [];
   let appointmentsArr = null;
   if (state.days.length === 0) {
-    console.log('Empty array');
     return [];
   }
   for (const dayObj of state.days) {
     if (dayObj.name === day) {
       appointmentsArr = dayObj.appointments;
-      console.log('appointmentsArr :', appointmentsArr);
     }
   }
 
