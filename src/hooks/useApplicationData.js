e5cda1d6c87e8673b450be1f8696ca748929c637
Shadee Merhi <shadeemerhi@Shadees-MacBook-Pro-2.local>
2020-10-29 18:31:03 -0600

    Spots Remaining now updates

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -6,6 +6,7 @@ export const useApplicationData = function() {
   const [state, setState] = useState({
     days: [],
     day: "Monday",
+
     appointments: {},
     interviewers: {}
   });
@@ -26,6 +27,22 @@ export const useApplicationData = function() {
     day
   });
 
+  const updateSpots = function(keyWord, day, days) {
+
+    const dayOfInterestArr = days.filter((currDay) => currDay.name === day); // Monday
+    const dayOfInterest = dayOfInterestArr[0];
+    if (keyWord == "Delete") {
+      dayOfInterest.spots += 1;
+    } else {
+      dayOfInterest.spots -= 1;
+    }
+    setState({
+      ...state,
+
+    })
+
+  }
+
   const cancelInterview = function(id) {
     const appointment = {
       ...state.appointments[id],
@@ -36,6 +53,8 @@ export const useApplicationData = function() {
       ...state.appointments,
       [id]: appointment
     }
+
+    updateSpots("Delete", state.day, state.days);
 
     return axios.delete(`/api/appointments/${id}`, {...appointment})
     .then(() => setState({
@@ -54,6 +73,9 @@ export const useApplicationData = function() {
       ...state.appointments,
       [id]: appointment
     };
+
+
+    updateSpots("Increase", state.day, state.days);
 
     return axios.put(`/api/appointments/${id}`, {...appointment})
     .then(() => setState({
