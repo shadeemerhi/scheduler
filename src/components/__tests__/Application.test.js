8439684908ab3c2b2c8e739be64b1c524af3f226
Shadee Merhi <shadeemerhi@Shadees-MacBook-Pro-2.local>
2020-11-02 16:56:20 -0700

    Added test to default to Monday and change schedule when new day selected

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,18 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
+describe("Renders without crashing", () => {
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    const { getByText } = render(<Application />);
+    return waitForElement(() => getByText("Monday")).then(() => {
+      const tuesdayBtn = getByText("Tuesday");
+      fireEvent.click(tuesdayBtn);
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    });
+  });
 });
