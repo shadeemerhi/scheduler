ba0036d21c403286ff1af82f098e74285875ebdf
Shadee Merhi <shadeemerhi@Shadees-MacBook-Pro-2.local>
2020-11-02 18:32:53 -0700

    Test initialized for loading data and booking interview

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, wait } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -14,5 +14,11 @@ describe("Application", () => {
       fireEvent.click(tuesdayBtn);
       expect(getByText("Leopold Silvers")).toBeInTheDocument();
     });
+  });
+
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+    const { container } = render(<Application />);
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+    console.log(prettyDOM(container));
   });
 });
